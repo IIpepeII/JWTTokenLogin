47abf9fdff97263cbfc36eb060a6d5585081ea21
IIpepeII <szrk.peter@gmail.com>
2024-12-19 14:29:52 +0100

    feat: working logout + some background

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 // MongoDB kapcsolat
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    // useUnifiedTopology: true
 }).then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('MongoDB connection error:', err));
 
